813e108777756dbaa577457bb986c440ec7eb75e
LorenaSTN <lorenstn@hotmail.com>
2025-05-16 18:16:44 +0200

    Filter component and PropTypes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,33 @@
 import "../scss/App.scss";
 import charactersFromApi from "./services/charactersFromApi";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Filter from "./Filter";
 
 function App() {
+  const [characters, setCharacters] = useState([]);
+  const [filterName, setFilterName] = useState("");
+  const [filtered, setFiltered] = useState([]);
 
-useEffect(() => {
+  useEffect(() => {
     charactersFromApi().then((charactersData) => {
-      console.log(charactersData)
+      console.log(charactersData);
+      setCharacters(charactersData);
+      setFiltered(charactersData);
+    });
+  }, []);
+
+  const handleSubmit = (ev) => {
+    ev.preventDefault(); 
+    const filteredCharacters = characters.filter((character) => {
+      return character.name.toLowerCase().includes(filterName.toLowerCase())
     })
-  }, [])
+    setFiltered(filteredCharacters);
+    console.log(filteredCharacters)
+  }
 
   return (
     <div>
+      <Filter valueName={filterName} onChangeName={setFilterName} onSubmit={handleSubmit} />
     </div>
   );
 }
--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,7 +1,51 @@
-function Filter() {
+import "../scss/main/Filter.scss";
+import PropTypes from "prop-types";
+
+function Filter({valueName, onChangeName, onSubmit}) {
+
+    const handleChangeName = (ev) => {
+        onChangeName(ev.target.value);
+    }
   return (
-    <div>Filter</div>
-  )
+    <div className="filter__container">
+
+      <div className="filter__header">
+        <h3 className="filter__header__title">Dragon Ball character finder</h3>
+        <img src="" alt="" />
+      </div>
+
+      <p className="filter__description">
+        What Dragon Ball character are you looking for?
+      </p>
+
+      <form className="filter__form" onSubmit={onSubmit}>
+        <div>
+          <label htmlFor="name">Search by name</label>
+          <input
+            className="input__name"
+            type="text"
+            id="name"
+            placeholder="Goku"
+            value={valueName}
+            onChange={handleChangeName}
+          />
+        </div>
+
+        <div>
+            <label htmlFor="ki">Search Ki range</label>
+            <div>
+            </div>
+        </div>
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
 }
 
-export default Filter
+export default Filter;
+
+Filter.propTypes = {
+    valueName: PropTypes.string.isRequired,
+    onChangeName: PropTypes.func.isRequired,
+    onSubmit: PropTypes.func.isRequired,
+}
